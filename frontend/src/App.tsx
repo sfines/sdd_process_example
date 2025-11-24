1d50985a9ced7e876b81fb0e6851688a77c467f7
Steven Fines <sfines@objectdriven.com>
2025-11-23 22:46:38 -0800

    fix: Keep WebSocket icon, remove toast notifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useSocket } from './hooks/useSocket.js';
 import Home from './pages/Home';
 import RoomView from './pages/RoomView';
-import Toast from './components/Toast';
-import { ConnectionStatus } from './components/ConnectionStatus';
 
 function AppContent(): JSX.Element {
   // Initialize socket connection (must be inside Router for useNavigate to work)
   useSocket();
 
   return (
-    <>
-      <div className="fixed top-4 right-4 z-50">
-        <ConnectionStatus />
-      </div>
-      <Toast />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/room/:roomCode" element={<RoomView />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/room/:roomCode" element={<RoomView />} />
+    </Routes>
   );
 }
 
